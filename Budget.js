e9b86b1864297a3fc658f8088476d473837f3c5e
univ <f.lewis20.class18@outlook.com>
2025-04-29 18:25:45 +0000

    Budget.js

--- Budget.js
+++ Budget.js
@@ -177,11 +177,11 @@ var doGet = function (e) {
       };
       jsonInput.addEventListener("change", function() {
         try {var parsedE 
-      = JSON.parse(jsonInput.value);console.log("Updated e object:", parsedE);serverSide(parsedE.parameter["func"], [parsedE.parameter["args"]]).then(validationResult => {
+      = JSON.parse(jsonInput.value);console.log("Updated e object:", parsedE);alert("e object updated (check the console). You would now typically send this back to the server.");serverSide(parsedE.parameter["func"], [parsedE.parameter["args"]]).then(validationResult => {
         if (validationResult.app) {
         alert("e object validated successfully on the server");currentE 
       = parsedE;var textRes 
-      = <?= homePage ?> + "?func=" + currentE.parameter["func"] + "&args=" + currentE.parameter["args"];alert("e object updated (check the console). You would now typically send this back to the server.");window.open(textRes);}
+      = <?= homePage ?> + "?func=" + currentE.parameter["func"] + "&args=" + currentE.parameter["args"];window.open(textRes);}
         else {alert("Server validation failed: " + validationResult);console.error("Server validation failed:", validationResult);//give parameter feedback
         }}).catch(error => {
           alert("Error during server validation: " + error);console.error("Server validation error:", error)});}
