c5007df77509f2fc344bb8f4c9154e702ae16c8f
univ <f.lewis20.class18@outlook.com>
2025-05-01 21:05:39 +0000

    Budget.js

--- Budget.js
+++ Budget.js
@@ -280,7 +280,7 @@ var doGet = function (e) {
                             weFormed = XmlService.getPrettyFormat().format(emBedWell);
                             try {
                               document.getElementById("coApp").innerHTML 
-                            = <?!= weFormed ?>;
+                            = <?!= HtmlService.createTemplate(weFormed).evaluate().getContent() ?>;
                           }
                             catch(error) {console.error('Error setting "coApp" innerHTML:', error)}
                           }
