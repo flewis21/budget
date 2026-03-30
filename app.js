caab71d3938893915d67f104479b70253f08e18c
univ <f.lewis20.class18@outlook.com>
2026-03-30 22:22:02 +0000

    app.js

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ function onYouTubeIframeAPIReady() {
   });
   function onPlayerReady(event) {
     event.target.loadPlaylist("UU6DOFpA9UCTgNwJiVX1IOpQ", ctr);
-    ctr++;
     event.target.setShuffle();
     event.target.setLoop();
     event.target.playVideo();
+    ctr++;
   }
 
   // 5. The API calls this function when the player's state changes.
@@ -41,21 +41,17 @@ function onYouTubeIframeAPIReady() {
   let done = false;
   function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.PLAYING) {
-      setTimeout(nextVideo);
       event.target.setShuffle();
       event.target.setLoop();
       ctr++;
-    }
-    else if (event.data == YT.PlayerState.UNSTARTED && !done) {
-      var youtubeID = event.target.getVideoUrl()
+    } else if (event.data == YT.PlayerState.UNSTARTED && !done) {
       changeBorderColor(event.data);
       setTimeout(playVideo);
       event.target.setShuffle();
       event.target.setLoop();
       event.target.playVideo();
       ctr++;
-    }
-    else if (event.data == YT.PlayerState.ENDED && !done) {
+    } else if (event.data == YT.PlayerState.ENDED && !done) {
       iframePlayer.loadPlaylist("UU6DOFpA9UCTgNwJiVX1IOpQ", ctr);
       setTimeout(nextVideo);
       changeBorderColor(event.data);
@@ -63,22 +59,17 @@ function onYouTubeIframeAPIReady() {
       event.target.setLoop();
       event.target.playVideo();
       ctr++;
-    }
-    else if (event.data == YT.PlayerState.PAUSED && !done) {
-      setTimeout(playVideo);
+    } else if (event.data == YT.PlayerState.PAUSED && !done) {
       changeBorderColor(event.data);
       event.target.setShuffle();
       event.target.setLoop();
       event.target.playVideo();
-    }
-    else if (event.data == YT.PlayerState.BUFFERING && !done) {
-      setTimeout(playVideo);
+    } else if (event.data == YT.PlayerState.BUFFERING && !done) {
       changeBorderColor(event.data);
       event.target.setShuffle();
       event.target.setLoop();
       event.target.playVideo();
-    }
-    else if (event.data == YT.PlayerState.VIDEO_CUED && !done) {
+    } else if (event.data == YT.PlayerState.VIDEO_CUED && !done) {
       setTimeout(playVideo);
       changeBorderColor(event.data);
       event.target.setShuffle();
@@ -92,26 +83,26 @@ function onYouTubeIframeAPIReady() {
     let color;
 
     if (playerStatus == -1) {
-      color = "#37474F"
+      color = "#37474F";
     } // unstarted = gray
     else if (playerStatus == 0) {
-      color = "#FFFF00"
+      color = "#FFFF00";
     } // ended = yellow
     else if (playerStatus == 1) {
-      color = "#33691E"
+      color = "#33691E";
     } // playing = green
     else if (playerStatus == 2) {
-      color = "#DD2C00"
+      color = "#DD2C00";
     } // paused = red
     else if (playerStatus == 3) {
-      color = "#AA00FF"
+      color = "#AA00FF";
     } // buffering = purple
     else if (playerStatus == 5) {
-      color = "#FF6DOO"
+      color = "#FF6DOO";
     } // video cued = orange
 
     if (color) {
-      document.getElementById('iframePlayer').style.borderColor = color
+      document.getElementById("iframePlayer").style.borderColor = color;
     }
   }
   function stopVideo() {
